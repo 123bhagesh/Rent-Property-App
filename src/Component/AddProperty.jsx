5669df11e1ebaeee69178561d2db4c230f8959b6
123bhagesh <123bhagesh@gmail.com>
2022-11-11 15:18:05 +0530

    data is maped

--- src/Component/AddProperty.jsx
+++ src/Component/AddProperty.jsx
@@ -10,31 +10,37 @@ export const AddProperty = () => {
   const [proptype, setProType] = useState('')
   const [name, setName] = useState('')
   const [image,setImage] = useState('')
-  const [beds,setBeds] = useState('')
+  // const [beds,setBeds] = useState('')
   const [bedroom, setBedroom] = useState('')
   const [city, setCity] = useState('')
   const [date, setDate] = useState('')
   const [area, setArea] = useState('')
+  const [rent, setRent] = useState('')
   const [address, setAddress] = useState('')
 
-  // console.log(proptype,name,beds,bedroom,city,date,area,address)
+
+  var rentArray=JSON.parse(localStorage.getItem("Rent_App_Data"))||[];
 
   const handelAdd =(e)=>{
     e.preventDefault()
-    console.log(proptype,name,beds,bedroom,city,date,area,address)
+    console.log(proptype,name,bedroom,city,date,area,address)
     // console.log("I am Bhagesh")
     let payload={
       "propertyType":proptype,
       "houseName": name,
       "image":image,
-      "noBeds":beds,
       "noBedroom":bedroom,
       "city":city,
       "date":date,
       "areaOfHouse":area,
-      "address":"address"
+      "rent":Number(rent),
+      "address":address
     }
     axios.post(`https://jsonserver-her-mock5.herokuapp.com/rent_property`,payload)
+    
+    // storing the data in Local storage
+    rentArray.push(payload)
+    localStorage.setItem("Rent_App_Data",JSON.stringify(rentArray))
 
   }
 
@@ -51,47 +57,42 @@ export const AddProperty = () => {
         </div>
         
         <div className={styles.formDiv}>
-           <h2 style={{marginTop:"-10px",marginBottom:"-5px"}}>Begin Posting your Property</h2>
+           <h2 style={{marginTop:"-5px",marginBottom:"5px"}}>Begin Posting your Property</h2>
            <form onSubmit={handelAdd}>
               <div>
                 <select className={styles.ProType}  onChange={(e)=>setProType(e.target.value)} required>
                   <option >Property Type </option>
-                  <option value="House">House/Villa</option>
-                  <option value="Flat">Flat/Apparment</option>
-                  <option value="Builder Floor">Independent/Builder Floor</option>
+                  <option value="Independent House">Independent House</option>
+                  <option value="Apartment">Apartment</option>
+                  <option value="Builder Floor">Builder Floor</option>
                 </select>
                 <input style={{width:"52%"}} type="text" placeholder='House or Apartment name...' onChange={(e)=>setName(e.target.value)} required/>
               </div>
               <input style={{width:"97%"}} type="text" placeholder='Add image of property...' onChange={(e)=>setImage(e.target.value)} required/>
-              <select onChange={(e)=> setBeds(e.target.value)} required>
-                <option value="">Number of Beds</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-              </select>
+
               <select onChange={(e)=> setBedroom(e.target.value)} required>
                 <option value="">Number of Bedrooms</option>
+                <option value="1">1</option>
                 <option value="2">2</option>
                 <option value="3">3</option>
                 <option value="4">4</option>
                 <option value="5">5</option>
               </select>
-              <select  placeholder='Select State' onChange={(e)=> setCity(e.target.value)} required>
+              <select onChange={(e)=> setCity(e.target.value)} required>
                   <option value={null}>Select City</option>
                   <option value='Pune'>Pune</option>
                   <option value='Delhi'>Delhi</option>
                   <option value='Mumbai'>Mumbai</option>
-                  <option value='Nagpur'>Nagpur</option>
               </select>
               <div className={styles.dateMain}>
                   <div className={styles.date}>
                     {
                       date.length <= 0 ? "Availability Date" : date
                     } 
-                    <input type="date" onChange={(e)=> setDate(e.target.value)} required/>
+                    <input type="date" onChange={(e)=> setDate(e.target.value)}   min={Date.now()} max="2023-01-28" required/>
                   </div>
-                  <input  type="number" placeholder="Bult up Area in square feet... " onChange={(e)=> setArea(e.target.value)} required/>
-                  <input style={{width:"120px",marginRight:"7px"}} type="number" placeholder='Rent Of Property...' />
+                  <input className={styles.area} type="number" placeholder="Bult up Area in square feet... " onChange={(e)=> setArea(e.target.value)} required/>
+                  <input style={{width:"125px",marginRight:"7px"}} type="number" placeholder='Rent of Property...' onChange={(e)=> setRent(e.target.value)} />
               </div>
               <input style={{width:"97%"}} type="text" placeholder='Address...' onChange={(e)=> setAddress(e.target.value)} required />
               <button type='submit' className={styles.postButton}>Post your Property</button>
--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import Styles from './Style/Home.module.css'
 import estateryicon from './estatery-icon.PNG'
 import { Navbar } from './Navbar'
+import axios from 'axios'
+import { getDefaultNormalizer } from '@testing-library/react'
 
 export const Home = () => {
+  const [data, setData] = useState([])
+
+  const getData = ()=>{
+     axios.get(`https://jsonserver-her-mock5.herokuapp.com/rent_property`)
+    .then((res)=> setData(res.data))
+  }
+  useEffect(()=>{
+     getData()
+  },[])
+  console.log(data)
+
   return (
     <div >
       <h1 className={Styles.Home}>Home Page</h1>
@@ -38,6 +51,24 @@ export const Home = () => {
 
         </select>
         
+      </div>
+      <div>
+       {
+        data?.map((el)=>(
+          // console.log(el.id)
+          <div key={el.id} className={Styles.fristDiv}>
+            <div>
+              <img style={{width:"100%"}} src={el.image} alt={el.image} />
+            </div>
+            <div>
+              <div style={{display:"flex"}}>
+                <h3>₹{el.rent} /</h3><p style={{marginTop:"21px"}}>month</p>
+              </div>
+            </div>
+
+          </div>
+        ))
+       }
       </div>
     </div>
   )
